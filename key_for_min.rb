ad136a51217e908a0b5b20f3f39c15cc642577ac
Josiah  Partridge <jpartridj94@gmail.com>
2020-05-07 02:44:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,5 @@
 def key_for_min_value(name_hash)
     if name_hash.empty? == true
       return nil
-    else name_hash.each do |key, value|
-      key_to_return = key
-      if value < key_to_return[value]
-        key_to_return = key
-      end
     end
-    key_to_return
-  end
 end
