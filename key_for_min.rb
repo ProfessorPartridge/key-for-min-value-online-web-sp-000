02028a483c0494e2a806930b445e40a842b616fc
Josiah  Partridge <jpartridj94@gmail.com>
2020-05-07 03:10:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if value < min_value
         min_value = value
-        key_for_min_value = min_value.key
+        key_for_min_value = name_hash.key(min_value)
       end
     end
     return key_for_min_value
