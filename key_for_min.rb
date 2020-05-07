ac180898f35c31313dd87eb9160ac71c68e09274
Josiah  Partridge <jpartridj94@gmail.com>
2020-05-07 02:46:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name, value)
+def key_for_min_value(name_hash)
     if name_hash.empty? == true
       return nil
     end
